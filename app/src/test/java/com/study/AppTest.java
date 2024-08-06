dde333af0ee57dfc711678d178862ea53c036c1b
Ubuntu <ubuntu@ip-172-32-14-36.ap-northeast-2.compute.internal>
2024-08-06 00:26:45 +0000

    initial commit

---
+++ app/src/test/java/com/study/AppTest.java
@@ -0,0 +1,14 @@
+package com.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+
